559272d7aa4144d1292c044e3ba2423f97c000ce
tiagowl <winkellandi123@gmail.com>
2023-01-29 15:38:36 -0300

    pacote de analitycs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/react";
+import {Analytics} from "@vercel/analytics/react";
 import Theme from "../styles/theme";
 import "../styles/font.css";
 import "../styles/scrollbar.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={Theme} >
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={Theme} >
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics/>
+    </>
   )
 }
