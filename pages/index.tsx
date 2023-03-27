a759b480c6017eddcea5b754a2ea913d50759ce4
tiagowl <winkellandi123@gmail.com>
2023-03-27 15:31:58 -0300

    otimização auth artigo 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import Posts from "../components/Posts";
 import Tools from "../components/Tools";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { useRouter } from "next/router";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
 
@@ -20,11 +21,14 @@ export default function Home() {
 
   const {isOpen, onToggle} = useDisclosure();
   const router = useRouter();
+  const { loginWithRedirect, user, isAuthenticated } = useAuth0();
 
   useEffect(()=>{
     if(typeof window !== "undefined"){
       setShow(true);
       if(sessionStorage.getItem("urlRedirect") != null){
+        localStorage.setItem("username", user?.name as string);
+        localStorage.setItem("user_avatar", user?.picture as string);
         router.push(sessionStorage.getItem("urlRedirect") as string);
         sessionStorage.removeItem("urlRedirect");
       }
