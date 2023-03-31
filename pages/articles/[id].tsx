37e3b1eef3d62943c96ef719d9bb1199e1e20837
tiagowl <winkellandi123@gmail.com>
2023-03-31 14:38:02 -0300

    correção meta tags

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -129,7 +129,7 @@ export default function Article(){
             <meta property="og:type" content="article" />
             <meta name="description" content={article?.data[0]?.attributes?.text?.substring(0, 298)} />
             <meta property="og:title" content={article?.data[0]?.attributes?.title}  />
-            <meta name="og:description" content={article?.data[0]?.attributes?.text?.substring(0, 298)} />
+            <meta property="og:description" content={article?.data[0]?.attributes?.text?.substring(0, 298)} />
         </Head>
         <Main>{loading ? <Spinner color="white" margin="0 auto" mt="5" /> : <>
         
